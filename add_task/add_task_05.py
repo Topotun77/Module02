ac659cf7c1ee0bdb907b171d6396d50d00c175ec
Svetlana <bel-svetik@yandex.ru>
2024-05-24 01:29:16 +0300

    Добавить 2 варианта расчета НОД без рекурсии

--- add_task/add_task_05.py
+++ add_task/add_task_05.py
@@ -1,16 +1,34 @@
+# Реализуйте в отдельной функции алгоритм Евклида (алгоритм нахождения наибольшего общего
+# делителя (НОД) пары целых чисел), возвращая НОД. Проверить полученную функцию на списке,
+# состоящем из 10 пар чисел (пример пар: [[5, 7], [21, 111], [63, 49]]).
+
 def evklid(a, b):
-    if a > b:
-        a, b = b, a
     new_a = b % a
     if new_a == 0:
         return a
     else:
-        return(evklid(new_a, a))
+        return evklid(new_a, a)
+
+
+def func_nod2(a, b):
+    while a and b:
+        a, b = b, a
+        b %= a
+    return a
+
+
+def func_nod(a, b):
+    while a != 0 and b != 0:
+        if a >= b:
+            a %= b
+        else:
+            b %= a
+    return a or b
 
 
 list_ = [[5, 7], [21, 111], [63, 49]]
 for l in list_:
-    print(f'НОД ({l[0]}, {l[1]}) = {evklid(*l)}')
+    print(f'НОД ({l[0]}, {l[1]}) = {evklid(*l)}\t\t{func_nod(*l)}\t\t{func_nod2(*l)}')
 
 a, b = map(int, input('\nВведите 2 числа через пробел: ').split())
-print(f'НОД ({a}, {b}) = {evklid(a, b)}')
+print(f'НОД ({a}, {b}) = {evklid(a, b)}\t\t{func_nod(a, b)}\t\t{func_nod2(a, b)}')
